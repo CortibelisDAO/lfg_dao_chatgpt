d79c1a51d677f9a61393e443838d9af228b602e2
ROI <121527735+CortibelisDAO@users.noreply.github.com>
2022-12-28 04:25:45 +0300

    new1

--- Wix/ToolResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PSMSI.Wix
-{
-    public class ToolResult
-    {
-        public string Output { get; set; }
-        public string Error { get; set; }
-        public bool Success { get; set; }
-    }
-}
